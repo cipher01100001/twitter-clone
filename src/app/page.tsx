9d640aa9b2319a78999faa5547403d164dd7a426
cipher <cipher01100001@gmail.com>
2024-03-08 15:51:20 -0500

    Toggle display of sign in/sign out button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
-export default function Home() {
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { AuthButtom } from "./components/auth-buttom-client";
+import { AuthButtomServer } from "./components/auth-buttom-server";
+
+export default async function Home() {
+  const supabase = createServerComponentClient({ cookies });
+  const { data: posts } = await supabase.from('posts').select('*');
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello World
+      <AuthButtomServer />
+      <pre>
+        {
+          JSON.stringify(posts, null, 2)
+        }
+      </pre>
     </main>
   );
 }
