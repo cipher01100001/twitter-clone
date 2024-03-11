ebdd72fc044852cbcfbbe7c62635849fde1d83cb
cipher <cipher01100001@gmail.com>
2024-03-11 12:29:54 -0500

    Clean up form after posting

---
+++ src/app/components/compose-post-textarea.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import { useEffect, useRef } from "react";
+import { useFormStatus } from "react-dom";
+
+export function ComposePostTextarea() {
+    const { pending } = useFormStatus();
+    const alreadySent = useRef(false);
+    const textAreaRef = useRef<HTMLTextAreaElement>(null);
+
+    useEffect(() => {
+        if (textAreaRef.current === null) return;
+
+        if (!pending && alreadySent.current) {
+            alreadySent.current = false;
+            textAreaRef.current.value = "";
+            return
+        }
+
+        alreadySent.current = pending;
+
+    }, [pending]);
+
+
+    return (
+        < textarea
+            ref={textAreaRef}
+            name='content'
+            rows={4}
+            className='w-full text-xl bg-black placeholder-gray-500 p-2'
+            placeholder="What's happening!?"
+        >
+        </ textarea >
+    )
+}
--- src/app/components/compose-post.tsx
+++ src/app/components/compose-post.tsx
@@ -2,6 +2,8 @@ import { Avatar } from '@nextui-org/react';
 import { createServerActionClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
 import { revalidatePath } from 'next/cache';
+import { ComposePostTextarea } from './compose-post-textarea';
+import { ComposePostButton } from './compose-post-button';
 
 export function ComposePost({
     userAvatarUrl
@@ -14,7 +16,7 @@ export function ComposePost({
 
         const content = formData.get('content');
 
-        if (content === null) return;
+        if (content === null || content === "") return;
 
         const supabase = createServerActionClient({ cookies })
 
@@ -30,16 +32,8 @@ export function ComposePost({
         <form action={addPost} className='flex flex-row p-3 border-b border-white/20'>
             <Avatar className='mr-2' isBordered radius="full" size="md" src={userAvatarUrl} />
             <div className='flex flex-1 flex-col gap-y-4'>
-                <textarea
-                    name='content'
-                    rows={4}
-                    className='w-full text-xl bg-black placeholder-gray-500 p-2'
-                    placeholder="What's happening!?"
-                >
-                </textarea>
-                <button type='submit' className='bg-sky-500 font-bold rounded-full px-5 py-2 self-end'>
-                    Post
-                </button>
+                <ComposePostTextarea />
+                <ComposePostButton />
             </div>
         </form>
     )
